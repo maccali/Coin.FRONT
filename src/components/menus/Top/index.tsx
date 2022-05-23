a420b38bda9de1670b17b7bba707567da64e26f3
maccali <guilherme.maccali@universo.univates.br>
2022-05-22 23:24:21 -0300

    feat: menu top

--- src/components/menus/Top/index.tsx
+++ src/components/menus/Top/index.tsx
@@ -1,11 +1,41 @@
-import { Block } from './styles';
+import { Block, Search, ButtonIcon, ButtonProfile } from './styles';
 
 
 export default function MenuTop() {
   return (
     <>
       <Block>
+        <div className='search'>
+          <Search>
+            <input type="text" />
+            <img src="/icons/search.png" alt="" />
+          </Search>
+        </div>
+        <div className='buttonIconConfig'>
+          <ButtonIcon >
+            <img src="/icons/config.png" alt="" />
+          </ButtonIcon>
+        </div>
+        <div className='buttonIconBell'>
+          <ButtonIcon>
+            <img src="/icons/bell.png" alt="" />
+          </ButtonIcon>
+        </div>
+        <div className='buttonIconProfile'>
+          <ButtonProfile>
+            <div>
+              <span>Olá,</span>
+              <b>Felipe</b>
+            </div>
+            <img src="/icons/felipe.png" alt="" />
+          </ButtonProfile>
+        </div>
+        <div className='buttonIconApp'>
+          <ButtonIcon>
+            <img src="/icons/app.png" alt="" />
 
+          </ButtonIcon>
+        </div>
       </Block >
     </>
   )
--- src/components/menus/Top/styles.ts
+++ src/components/menus/Top/styles.ts
@@ -1,9 +1,90 @@
 import styled from "styled-components";
 
 export const Block = styled.div`
-  display: flex;
-  width: 100%;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-areas: "search buttonIconConfig buttonIconBell buttonIconProfile buttonIconApp";
+  grid-template-columns: 1fr 40px 40px 160px 40px;
   height: 88px;
   background: #ffffff;
   border-radius: 30px 30px 0px 0px;
+  padding-right: 56px;
+  padding-left: 32px;
 `;
+
+export const Search = styled.div`
+  display: flex;
+  align-items: center;
+
+  input {
+    position: absolute;
+    width: 400px;
+    height: 40px;
+    background: #f5f5f5;
+    border-radius: 5px;
+    border: 0;
+    padding-left: 37px;
+    font-family: "Open Sans";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 19px;
+  }
+
+  img {
+    position: relative;
+    margin-left: 12.5px;
+  }
+`;
+
+export const ButtonIcon = styled.button`
+  background-color: transparent;
+  border: 0;
+`;
+
+export const ButtonProfile = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 150px;
+  height: 56px;
+  background: rgba(47, 68, 134, 0.05);
+  border-radius: 50px;
+  border: 0;
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 19px;
+  padding-left: 10px;
+  padding-right: 10px;
+  color: rgba(86, 84, 104, 0.8);
+`;
+
+// <div className='search'>
+// <Search></Search>
+// </div>
+// <div className='buttonIconConfig'>
+// <ButtonIcon >
+//   <img src="/icons/config.png" alt="" />
+// </ButtonIcon>
+// </div>
+// <div className='buttonIconBell'>
+// <ButtonIcon>
+//   <img src="/icons/bell.png" alt="" />
+// </ButtonIcon>
+// </div>
+// <div className='buttonIconProfile'>
+// <ButtonProfile>
+//   <span></span>
+//   <b></b>
+//   <img src="/icons/felipe.png" alt="" />
+// </ButtonProfile>
+// </div>
+// <div className='buttonIconApp'>
+// <ButtonIcon>
+//   <img src="/icons/app.png" alt="" />
+
+// </ButtonIcon>
+// </div>
